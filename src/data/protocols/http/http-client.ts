6c6e7e0d7ed3f54f7d86a0f540ed944cf33f29de
Gustavo Pessa <ghmpessa@gmail.com>
2023-01-19 23:03:21 -0300

    feat: add http protocol

---
+++ src/data/protocols/http/http-client.ts
@@ -0,0 +1,24 @@
+export type HttpRequest = {
+  url: string
+  method: HttpMethod
+  body?: any
+  headers?: any
+  params?: any
+}
+
+export interface HttpClient<R = any> {
+  request: (data: HttpRequest) => Promise<HttpResponse<R>>
+}
+
+export type HttpMethod = 'post' | 'get'
+
+export enum HttpStatusCode {
+  ok = 200,
+  unauthorized = 401, 
+  notFound = 404,
+}
+
+export type HttpResponse<T = any> = {
+  statusCode: HttpStatusCode
+  body?: T
+}
