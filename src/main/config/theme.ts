7596c71c4b73bfe44a51e2e66fba6c4720a741e4
Gustavo Pessa <ghmpessa@gmail.com>
2023-01-20 12:22:56 -0300

    feat: add application theme

---
+++ src/main/config/theme.ts
@@ -0,0 +1,32 @@
+import { createTheme } from '@mui/material/styles'
+
+export const theme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#AB2F2E',
+      light: '#B54E4D',
+      dark: '#320000',
+      contrastText: '#FFF',
+    },
+    secondary: {
+      main: '#949494',
+    },
+    background: {
+      default: '#171717',
+      paper: '#111111',
+    },
+    text: {},
+  },
+  typography: {
+    // fontFamily: '"Lato", "Roboto", "Helvetica", sans-serif',
+    fontSize: 14,
+    fontWeightLight: 200,
+    fontWeightRegular: 400,
+    fontWeightBold: 700,
+
+    button: {
+      fontWeight: 600,
+    },
+  },
+})
