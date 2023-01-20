3336e76515b0688880dca07386c1a3db7949d880
Gustavo Pessa <ghmpessa@gmail.com>
2023-01-20 20:49:18 -0300

    chore: config vite to resolve modules

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import alias from '@rollup/plugin-alias'
+import { resolve } from 'path'
 
-// https://vitejs.dev/config/
+const projectRootDir = resolve(__dirname)
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    alias({
+      entries: [
+        {
+          find: '@',
+          replacement: resolve(projectRootDir, 'src'),
+        },
+      ],
+    }),
+  ],
 })
