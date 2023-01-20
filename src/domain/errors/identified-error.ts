50221887b6a848876fb603facad3e39077fe3007
Gustavo Pessa <ghmpessa@gmail.com>
2023-01-20 02:04:21 -0300

    feat: add IdentifiedError

---
+++ src/domain/errors/identified-error.ts
@@ -0,0 +1,10 @@
+export abstract class IdentifiedError extends Error {
+  constructor(
+    public readonly type: string,
+    public readonly name: string,
+    message: string,
+    public readonly data?: unknown
+  ) {
+    super(message)
+  }
+}
