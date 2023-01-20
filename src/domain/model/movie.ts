bffdefebd48646c81ba7b525cf8a7b1387dc7957
Gustavo Pessa <ghmpessa@gmail.com>
2023-01-20 00:50:13 -0300

    refactor: add missing id to Movie model

--- src/domain/model/movie.ts
+++ src/domain/model/movie.ts
@@ -4,7 +4,8 @@ export namespace Movie {
     adult?: boolean
     overview?: string
     release_date?: string
-    genre_ids?: number[]
+    genre_ids?: Array<number>
+    id?: number
     original_title?: string
     original_language?: string
     title?: string
