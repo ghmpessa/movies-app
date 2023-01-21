5f0fbbf5b55d2cc6f337e0b1feecd8a6442a1bda
Gustavo Pessa <ghmpessa@gmail.com>
2023-01-20 21:50:43 -0300

    feat: add RemoteRequestToken to Header

--- src/main/routes/router.tsx
+++ src/main/routes/router.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
+import { makeRemoteRequestToken } from '@/main/factories/usecases'
+
 import { Footer, Header, MainContainer } from '@/presentation/components'
 import { Home } from '@/presentation/pages'
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 const Router: React.FC = () => {
   return (
     <BrowserRouter>
-      <Header />
+      <Header requestToken={makeRemoteRequestToken()} />
       <MainContainer>
         <Routes>
           <Route path='/' element={<Home />} />
--- src/presentation/components/header/index.tsx
+++ src/presentation/components/header/index.tsx
@@ -3,8 +3,22 @@ import { HeaderStyles as Styled } from './styles'
 
 import BookmarkIcon from '@mui/icons-material/Bookmark'
 import LocalMoviesRoundedIcon from '@mui/icons-material/LocalMoviesRounded'
+import { RequestToken } from '@/domain/usecases'
 
-const Header: React.FC = () => {
+type Props = {
+  requestToken: RequestToken
+}
+
+const Header: React.FC<Props> = ({ requestToken }) => {
+  const handleRequest = async () => {
+    try {
+      const response = await requestToken.request()
+      console.log(response)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <Styled.Container>
       <Styled.LogoContainer>
@@ -19,7 +33,9 @@ const Header: React.FC = () => {
           <BookmarkIcon sx={{ color: 'white' }} />
           <Styled.NavLink>Watch List</Styled.NavLink>
         </Styled.NavLinkContainer>
-        <Styled.Button variant='outlined'>Login</Styled.Button>
+        <Styled.Button variant='outlined' onClick={handleRequest}>
+          Login
+        </Styled.Button>
       </Styled.NavContainer>
     </Styled.Container>
   )
