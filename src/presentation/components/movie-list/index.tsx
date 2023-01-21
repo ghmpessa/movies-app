ad12eee5360f7ff19f72849034c13ff6d7cd428a
Gustavo Pessa <ghmpessa@gmail.com>
2023-01-21 11:11:14 -0300

    refactor: add empty message in case movie list is empty

--- src/presentation/components/movie-list/index.tsx
+++ src/presentation/components/movie-list/index.tsx
@@ -2,17 +2,29 @@ import React from 'react'
 import { MovieListStyles as Styled } from './styles'
 import { Movie } from '@/domain/models'
 import { MovieCard } from '@/presentation/components/'
+import { useNavigate } from 'react-router-dom'
 
 type Props = {
   movies: Array<Movie.ShortModel>
 }
 
 const MovieList: React.FC<Props> = ({ movies }) => {
+  const navigate = useNavigate()
   return (
     <Styled.Container>
-      {movies.map(movie => (
-        <MovieCard key={movie.id! * Math.random()} movie={movie} />
-      ))}
+      {movies.length > 0 ? (
+        movies.map(movie => (
+          <MovieCard key={movie.id! * Math.random()} movie={movie} />
+        ))
+      ) : (
+        <Styled.EmptyList>
+          <h3>Your watchlist is empty</h3>
+          <h3>Show us some movies you want to watch</h3>
+          <Styled.Button variant='contained' onClick={() => navigate('/')}>
+            Go to movies
+          </Styled.Button>
+        </Styled.EmptyList>
+      )}
     </Styled.Container>
   )
 }
--- src/presentation/components/movie-list/styles.ts
+++ src/presentation/components/movie-list/styles.ts
@@ -1,4 +1,4 @@
-import { styled } from '@mui/material'
+import { styled, Button as MUIButton } from '@mui/material'
 
 export namespace MovieListStyles {
   export const Container = styled('div')`
@@ -9,5 +9,24 @@ export namespace MovieListStyles {
     flex-wrap: wrap;
     width: 100%;
     max-width: 1500px;
+  `
+
+  export const EmptyList = styled('div')`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-around;
+
+    h3 {
+      font-size: 1.5rem;
+      margin: 5px;
+      font-weight: 500;
+    }
+  `
+
+  export const Button = styled(MUIButton)`
+    width: 200px;
+    padding: 12px 24px;
+    margin: 2rem;
   `
 }
