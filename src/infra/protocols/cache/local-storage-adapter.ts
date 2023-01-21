6adc6133613e17879d1e44b6e97fa2377b764821
Gustavo Pessa <ghmpessa@gmail.com>
2023-01-21 01:13:48 -0300

    fix: change LocalStorage to use setItem

--- src/infra/protocols/cache/local-storage-adapter.ts
+++ src/infra/protocols/cache/local-storage-adapter.ts
@@ -3,7 +3,7 @@ import { GetStorage, SetStorage } from '@/data/protocols/cache'
 export class LocalStorageAdapter implements SetStorage, GetStorage {
   set(key: string, value: any): void {
     if (!value) localStorage.removeItem(key)
-    else localStorage.set(key, JSON.stringify(value))
+    else localStorage.setItem(key, JSON.stringify(value))
   }
 
   get(key: string) {
