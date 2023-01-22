946a84b36f0a916ce1996e568ce03ce199ec95d3
Gustavo Pessa <ghmpessa@gmail.com>
2023-01-22 15:37:04 -0300

    feat: add LoadRatedMovies usecase

---
+++ src/domain/usecases/rating/load-rated-movies.ts
@@ -0,0 +1,17 @@
+import { Movie } from '@/domain/models'
+
+export interface LoadRatedMovies {
+  load(params: LoadRatedMovies.Params): Promise<LoadRatedMovies.Model>
+}
+
+export namespace LoadRatedMovies {
+  export type Model = {
+    results: Array<Movie.Model>
+    page: number
+    total_pages: number
+    total_results: number
+  }
+  export type Params = {
+    sessionId: TemplateStringsArray
+  }
+}
