a9a827254bd119f4c0b73a41b32a15c16925ed36
Gustavo Pessa <ghmpessa@gmail.com>
2023-01-22 21:46:09 -0300

    refactor: change RemotLoadMovies to receive path param

--- src/main/factories/usecases/movies/load-movies/remote-load-movies-factory.ts
+++ src/main/factories/usecases/movies/load-movies/remote-load-movies-factory.ts
@@ -2,5 +2,5 @@ import { makeApiUrl, makeAxiosHttpClient } from '@/main/factories/http'
 import { RemoteLoadMovies } from '@/data/usecases/movies'
 import { LoadMovies } from '@/domain/usecases'
 
-export const makeRemoteLoadMovies = (): LoadMovies =>
-  new RemoteLoadMovies(makeApiUrl('/discover/movie'), makeAxiosHttpClient())
+export const makeRemoteLoadMovies = (path: string): LoadMovies =>
+  new RemoteLoadMovies(makeApiUrl(path!), makeAxiosHttpClient())
--- src/main/factories/usecases/movies/load-popular-movies/remote-load-popular-movies-factory.ts
+++
@@ -1,6 +0,0 @@
-import { makeApiUrl, makeAxiosHttpClient } from '@/main/factories/http'
-import { RemoteLoadMovies } from '@/data/usecases/movies'
-import { LoadMovies } from '@/domain/usecases'
-
-export const makeRemoteLoadPopularMovies = (): LoadMovies =>
-  new RemoteLoadMovies(makeApiUrl('/movie/popular'), makeAxiosHttpClient())
