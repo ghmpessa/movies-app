c1138f4b0b58e4e16c28a10d9b16d851139c3db1
Gustavo Pessa <ghmpessa@gmail.com>
2023-01-22 21:43:30 -0300

    feat: add SearchPage

---
+++ src/presentation/pages/search/index.tsx
@@ -0,0 +1,95 @@
+import React, { useEffect, useState, useCallback, useRef } from 'react'
+import { SearchPageStyles as Styled } from './styles'
+
+import {
+  Error,
+  ScrollToTopFab,
+  Loading,
+  MovieList,
+  MovieListsHeader,
+} from '@/presentation/components'
+import { LoadMovies, SearchMovies } from '@/domain/usecases'
+import { useParams } from 'react-router-dom'
+
+type Props = {
+  searchMovies: SearchMovies
+}
+
+const SearchPage: React.FC<Props> = ({ searchMovies }) => {
+  const { query } = useParams()
+  const [data, setData] = useState<LoadMovies.Model>({
+    results: [],
+    page: 1,
+    total_pages: 0,
+    total_results: 0,
+  })
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(false)
+
+  const showLoadMore =
+    !loading && data.results.length > 0 && data.total_pages !== data.page
+
+  const fetchMovies = async (page = 1) => {
+    try {
+      const response = await searchMovies.search({ query: query || '', page })
+      setData(response)
+    } catch (error) {
+      setError(true)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    let isMounted = true
+
+    if (isMounted) fetchMovies()
+
+    return () => {
+      isMounted = false
+    }
+  }, [query])
+
+  return (
+    <Styled.Container>
+      <MovieListsHeader />
+      {loading && (
+        <Styled.LoadingContainer>
+          <Loading />
+        </Styled.LoadingContainer>
+      )}
+      {!loading && !error && (
+        <MovieList
+          movies={data ? data.results : []}
+          emptyList={{
+            title: 'This movie is not released yet',
+            buttonTitle: 'Clear search',
+          }}
+          // handleClick={() => fetchMovies()}
+        />
+      )}
+      {!loading && error && (
+        <Error
+          onTryAgain={() => {
+            setLoading(true)
+            // fetchMovies()
+          }}
+        />
+      )}
+      {showLoadMore && (
+        <Styled.LoadMore
+          variant='contained'
+          color='primary'
+          // onClick={() =>
+          //   query.length > 0 ? handleSearchMovie() : fetchMovies(data.page + 1)
+          // }
+        >
+          Load more
+        </Styled.LoadMore>
+      )}
+      <ScrollToTopFab />
+    </Styled.Container>
+  )
+}
+
+export default SearchPage
---
+++ src/presentation/pages/search/styles.ts
@@ -0,0 +1,59 @@
+import {
+  styled,
+  Button as MUIButton,
+  OutlinedInput as MUITextField,
+  Fab as MUIFab,
+} from '@mui/material'
+
+export namespace SearchPageStyles {
+  export const Container = styled('div')`
+    flex: 1;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+  `
+
+  export const LoadingContainer = styled('div')`
+    flex: 1;
+    flex-direction: column;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-height: calc(100vh - 115px);
+  `
+
+  export const LoadMore = styled(MUIButton)`
+    width: 150px;
+    align-self: center;
+    margin: 20px 0px 40px;
+  `
+
+  export const SearchContainer = styled('div')`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    align-self: flex-start;
+    margin-top: 3rem;
+
+    @media (max-width: 750px) {
+      align-self: center;
+    }
+  `
+
+  export const Input = styled(MUITextField)`
+    color: ${({ theme }) => theme.palette.common.white};
+    background-color: ${({ theme }) => theme.palette.background.paper};
+  `
+
+  export const SearchFab = styled(MUIFab)`
+    margin-left: 10px;
+    width: 52px;
+    height: 52px;
+
+    &.Mui-disabled {
+      background-color: ${({ theme }) => theme.palette.primary.main};
+    }
+  `
+}
