0827985cfb6ad25c526d7d301abd1708eab22f07
Gustavo Pessa <ghmpessa@gmail.com>
2023-01-23 22:53:45 -0300

    chore: remove build.minify terser from vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,6 @@ import { resolve } from 'path'
 const projectRootDir = resolve(__dirname)
 
 export default defineConfig({
-  build: {
-    minify: 'terser',
-  },
   plugins: [
     react(),
     alias({
